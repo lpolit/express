0f6422890bfce77f61879c9c8fdaf9fbbc1ed193
lpolit <leandrofpolitano@hotmail.com>
2024-05-23 23:09:01 -0300

    agrego endpoint /

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.get("/", (req, res) => {
+  const umTitulo = "Estoy en la raiz";
+  // Se le pasan datos desde el servidor al html
+  res.render("index", { titulo: umTitulo });
+});
+
+
 app.get("/info", (req, res) => {
   const umTitulo = "Meu página criada desde Express";
   // Se le pasan datos desde el servidor al html
   res.render("index", { titulo: umTitulo });
 });
-
 app.listen(3000);
 console.log("Servidor no porto 3000");
